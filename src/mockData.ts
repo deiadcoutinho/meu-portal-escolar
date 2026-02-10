f952b0fa56602f3af1cf14d41becbc1321f88292
deiadcoutinho <deia.d.coutinho@gmail.com>
2026-02-09 22:15:55 -0300

    Create mockData.ts

---
+++ src/mockData.ts
@@ -0,0 +1,5 @@
+import { Student } from './types';
+export const MOCK_STUDENTS: Student[] = [
+  { id: 's1', name: 'Gustavo Kaufman', password: '123', avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Gustavo', xp: 250, exercises: {} },
+  { id: 's2', name: 'Bruno Souza', password: '123', avatar: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Bruno', xp: 120, exercises: {} }
+];
